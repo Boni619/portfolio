745465ce0d1e1e2eaf5e0bdbccd3c6bab811e5cf
root <boni619@gmail.com>
2019-05-11 15:40:45 +0530

    Updates

---
+++ precache-manifest.d6f5fa2b9775fb8fc9f25c776099e671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f88e178a1602c594b652a014ede4665",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a47a6b641de30a95d4a8",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c60fa686a07f5b7389c1",
+    "url": "/portfolio/static/js/2.ca4fedf9.chunk.js"
+  },
+  {
+    "revision": "a47a6b641de30a95d4a8",
+    "url": "/portfolio/static/js/main.50967fa3.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
